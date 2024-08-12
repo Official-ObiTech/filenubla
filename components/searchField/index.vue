98b8e5a49b0d3a424b479e4e7b43fb02c995b8b1
Obinna-Nwankwo <official.nwankwo.840@gmail.com>
2024-08-12 13:18:00 +0100

    add: create an avatar on the big screen and the hidden on the small screen

--- components/searchField/index.vue
+++ components/searchField/index.vue
@@ -34,7 +34,6 @@
               </h3>
               <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1" @click="isOpen = false" />
             </div>
-          </template>
 
           <Placeholder class="h-32" />
         </UCard>
